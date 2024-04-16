f6ebd6dd15f5e408081bc5215b0bf666e1170fd5
Michał Czyż <mike@c2yz.com>
2024-04-16 21:38:35 +0200

    variable typo

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
   } @ inputs: let 
     inherit (self) outputs;
     # Supported systems for your flake packages, shell, etc.
-    system = [
+    systems = [
       "aarch64-linux"
       "i686-linux"
       "x86_64-linux"
