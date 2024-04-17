5abbb6d27efa2da2b7cf036a800ad660035c22ce
Michał Czyż <mike@c2yz.com>
2024-04-17 10:04:55 +0200

    hyprland initial config

---
+++ home-manager/features/desktop/common/default.nix
@@ -0,0 +1,15 @@
+{lib, pkgs, config, ...}: {
+  imports = [
+    
+  ];
+  
+  dconf.settings."org/gnome/desktop/interface".color-scheme = 
+    if config.colorscheme.variant == "dark"
+    then "prefer-dark"
+    else if config.colorscheme.variant == "light"
+    then "prefer-light"
+    else "default";
+
+  xdg.portal.enable = true;
+
+}
