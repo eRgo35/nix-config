e4d3cba5a30102e8e15eb40957c150bdc28b32e8
Michał Czyż <mike@c2yz.com>
2024-04-27 18:55:11 +0200

    ssh x11 forwarding

--- hosts/common/services.nix
+++ hosts/common/services.nix
@@ -64,6 +64,7 @@
       PermitRootLogin = "no";
       # Use keys only. Remove if you want to SSH using password (not recommended)
       PasswordAuthentication = true;
+      X11Forwarding = true;
     };
   };
 
