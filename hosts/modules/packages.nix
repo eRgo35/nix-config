49f4b87e74e108ac14d4efca59587b46bc893ef2
Michał Czyż <mike@c2yz.com>
2024-11-26 19:25:49 +0100

    feat: added teamviewer

--- hosts/modules/packages.nix
+++ hosts/modules/packages.nix
@@ -7,9 +7,12 @@
     nix-output-monitor
     nvd
     brightnessctl
+    teamviewer
   ];
 
   programs.nix-ld.enable = true;
+
+  services.teamviewer.enable = true;
 
   services.zerotierone = {
     enable = true;
