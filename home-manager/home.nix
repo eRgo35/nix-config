c893411fb852d717368b44da25ba801f1130c989
Michał Czyż <mike@c2yz.com>
2025-01-05 19:57:48 +0100

    feat: readded stylix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,81 +1,3 @@
-# {
-#   hostname,
-#   inputs,
-#   ...
-# }: {
-#   imports =
-#     if hostname == "zion"
-#     then [
-#       ./programs/btop.nix
-#       ./programs/fastfetch.nix
-#       # ./programs/foot.nix
-#       ./programs/git.nix
-#       ./programs/gpg.nix
-#       ./programs/helix.nix
-#       ./programs/kitty.nix
-#       ./programs/neovide.nix
-#       ./programs/neovim.nix
-#       ./programs/packages.nix
-#       ./programs/shell.nix
-#       ./programs/tmux.nix
-#       ./programs/vscodium.nix
-#
-#       ./dwm
-#       # ./programs/yazi.nix
-#       # ./programs/zathura.nix
-#
-#       # ./configs/keys.nix
-#
-#       ./system/dunst.nix
-#       ./system/hypr.nix
-#       ./system/hyprland.nix
-#       ./system/rofi.nix
-#       ./system/stylix.nix
-#       ./system/theme.nix
-#       ./system/waybar.nix
-#       ./system/wlsunset.nix
-#       ./system/wofi.nix
-#     ]
-#     else if hostname == "thor"
-#     then [
-#       ./programs/btop.nix
-#       ./programs/fastfetch.nix
-#       # ./programs/foot.nix
-#       ./programs/git.nix
-#       ./programs/gpg.nix
-#       ./programs/helix.nix
-#       ./programs/kitty.nix
-#       ./programs/neovide.nix
-#       ./programs/neovim.nix
-#       ./programs/packages.nix
-#       ./programs/shell.nix
-#       ./programs/tmux.nix
-#       ./programs/vscodium.nix
-#       # ./programs/yazi.nix
-#       # ./programs/zathura.nix
-#
-#       # ./configs/keys.nix
-#
-#       ./system/dunst.nix
-#       ./system/hyprland.nix
-#       ./system/rofi.nix
-#       ./system/stylix.nix
-#       ./system/theme.nix
-#       ./system/waybar.nix
-#       ./system/wlsunset.nix
-#       ./system/wofi.nix
-#     ]
-#     else if hostname == "server"
-#     then [
-#       ./programs/git.nix
-#       ./programs/btop.nix
-#       ./programs/yazi.nix
-#       ./programs/shell.nix
-#       ./programs/helix.nix
-#       ./programs/fastfetch.nix
-#     ]
-#     else [];
-# }
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
@@ -117,10 +39,10 @@
     # ./configs/keys.nix
 
     ./system/dunst.nix
-    # ./system/hypr.nix
+    ./system/hypr.nix
     # ./system/hyprland.nix
     ./system/rofi.nix
-    # ./system/stylix.nix
+    ./system/stylix.nix
     ./system/theme.nix
     ./system/waybar.nix
     ./system/wlsunset.nix
